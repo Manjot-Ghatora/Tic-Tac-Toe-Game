e74ee54cf576d9cf4e34f88d4cf3db86969a09f0
Manjot Ghatora <ghatoramanjotca@gmail.com>
2025-01-22 01:12:25 -0500

    Winner Name Capitalized

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -2,7 +2,7 @@ export default function GameOver({winner , rematch}){
     return <>
         <div id="game-over">
             <h2>  Game Over </h2>
-            {winner && <p>{winner} won!</p>}
+            {winner && <p>{winner.toUpperCase()} won !</p>}
             {!winner &&<p> It&apos;s a draw </p>}
 
             <button onClick={rematch}>Rematch</button>
